b774b70924d4b714e7ecc95e5a5b2f356f7aba93
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-09-22 14:12:04 +0100

    created git ignore and create another React element and added the other two to it as children

--- src/index.js
+++ src/index.js
@@ -12,13 +12,29 @@ serviceWorker.unregister();
 const title = React.createElement(
     'h1',
     {id: 'main-title', title: 'This is a title.'},
+
     'My First React Element!'
 );
 
-ReactDOM.render(
+const desc = React.createElement(
+
+    'p',
+    null,
+    'I just learned how to create a React node and Render' +
+    ' it into the DOM.'
+
+);
+
+const header = React.createElement(
+    'header',
+    null,
     title,
+    desc
+);
+
+ReactDOM.render(
+    header,
     document.getElementById('root')
 
 );
 
-console.log(title);
